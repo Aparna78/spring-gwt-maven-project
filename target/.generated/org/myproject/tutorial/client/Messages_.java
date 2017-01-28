b386d27e9efa035f6f82a05d827185bb2c524e83
aparna <aparnanit90@gmail>
2017-01-28 16:33:50 +0530

    adding full project file

---
+++ target/.generated/org/myproject/tutorial/client/Messages_.java
@@ -0,0 +1,12 @@
+package org.myproject.tutorial.client;
+
+public class Messages_ implements org.myproject.tutorial.client.Messages {
+  
+  public java.lang.String nameField() {
+    return "Enter your name";
+  }
+  
+  public java.lang.String sendButton() {
+    return "Send";
+  }
+}
